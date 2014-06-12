69884309527500bc02cf07799dc48ade72d510c8
lsmith17 <lsmith17@my.whitworth.edu>
2014-06-11 23:07:25 -0700

    Fixed Code

--- EX05_03/EX05_03/Vector.h
+++ EX05_03/EX05_03/Vector.h
@@ -11,13 +11,63 @@ private:
 	T elements[100];
 	int vectorSize;
 public:
-	Vector();
-	void push_back(T element);
-	void pop_back();
-	unsigned int size();
-	bool empty();
-	T at(int a);
-	void clear();
-	void swap(Vector v2);
+	Vector()
+	{
+		vectorSize = 0;
+	}
+
+	template<typename T>
+	bool empty()
+	{
+		return vectorSize = 0;
+	}
+
+	template<typename T>
+	T at(int a)
+	{
+		return elements[a];
+	}
+
+	template<typename T>
+	void push_back(T value)
+	{
+		elements[vectorSize++] = value;
+	}
+
+	template<typename T>
+	void pop_back()
+	{
+		return elements[--vectorSize];
+	}
+
+	template<typename T>
+	unsigned int size()
+	{
+		return vectorSize;
+	}
+
+	template<typename T>
+	void clear()
+	{
+		vectorSize = 0;
+	}
+
+	template<typename T>
+	void swap(Vector v2)
+	{
+		T temp[100];
+		int tempSize = v2.size();
+		for (int i = 0; i < v2.size(); i++)
+			temp[i] = v2.at(i);
+
+		v2.clear();
+		for (int i = 0; i < size(); i++)
+			v2.push_back(at(i));
+
+		clear();
+		for (int i = 0; i < tempSize; i++)
+			push_back(temp[i]);
+	}
+
 };
 #endif 
